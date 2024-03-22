ac100d8bcb47d70bd5a6d0d4971b1e2beb13fe00
alexcolbeck1 <36191568+alexcolbeck1@users.noreply.github.com>
2024-03-22 17:01:30 +0000

    First iteration almost finished

---
+++ app/data/session-data-defaults.js
@@ -0,0 +1,23 @@
+module.exports = {
+
+    "folders": "0",
+    "folder-name-1": "Health campaign Mar 2024",
+
+    "folder-1-app-templates": "0",
+    "folder-1-email-templates": "0",
+    "folder-1-sms-templates": "0",
+    "folder-1-letter-templates": "0",
+
+    "template-email-name-1": "National under 16s cohort",
+    "template-email-subject-1": "The subject line for the email",
+    "template-email-message-1": "Here is the message body text.",
+
+    "template-sms-name-1": "Text message for 30 to 50 yr olds",
+    "template-sms-message-1": "Here is the message body text.",
+
+    "template-letter-name-1": "Letter for awareness campaign 60+",
+    "template-letter-sender-1": "NHS organisation name, 25 Long Lane, Leeds, LS1 2AB",
+    "template-letter-heading-1": "The main heading of the letter",
+    "template-letter-message-1": "Here is the message body."
+  
+  }
--- app/routes.js
+++ app/routes.js
@@ -3,6 +3,74 @@ const express = require('express');
 
 const router = express.Router();
 
+// Logging session data  
+  
+router.use((req, res, next) => {    
+    const log = {  
+      method: req.method,  
+      url: req.originalUrl,  
+      data: req.session.data  
+    }  
+    console.log(JSON.stringify(log, null, 2))  
+   
+  next()  
+})
+
+// GET SPRINT NAME - useful for relative templates  
+router.use('/', (req, res, next) => {  
+res.locals.currentURL = req.originalUrl; //current screen  
+res.locals.prevURL = req.get('Referrer'); // previous screen
+
+console.log('folder : ' + res.locals.folder + ', subfolder : ' + res.locals.subfolder  );
+
+next();  
+});
+
 // Add your routes here - above the module.exports line
 
 module.exports = router;
+
+// Get the data values from session-data-defaults.js to use on the page
+
+router.get('*/manage', function (request, response) {
+  response.locals.defaults = require('./data/session-data-defaults.js')
+  response.render('manage/index')
+})
+
+router.get('*/folder-1', function (request, response) {
+  response.locals.defaults = require('./data/session-data-defaults.js')
+  response.render('manage/folder-1/index')
+})
+
+
+router.get('*/create-email-template', function (request, response) {
+  response.locals.defaults = require('./data/session-data-defaults.js')
+  response.render('manage/folder-1/create-email-template')
+})
+
+router.get('*/template-email-1', function (request, response) {
+  response.locals.defaults = require('./data/session-data-defaults.js')
+  response.render('manage/folder-1/template-email-1')
+})
+
+
+router.get('*/create-sms-template', function (request, response) {
+  response.locals.defaults = require('./data/session-data-defaults.js')
+  response.render('manage/folder-1/create-sms-template')
+})
+
+router.get('*/template-sms-1', function (request, response) {
+  response.locals.defaults = require('./data/session-data-defaults.js')
+  response.render('manage/folder-1/template-sms-1')
+})
+
+
+router.get('*/create-letter-template', function (request, response) {
+  response.locals.defaults = require('./data/session-data-defaults.js')
+  response.render('manage/folder-1/create-letter-template')
+})
+
+router.get('*/template-letter-1', function (request, response) {
+  response.locals.defaults = require('./data/session-data-defaults.js')
+  response.render('manage/folder-1/template-letter-1')
+})
