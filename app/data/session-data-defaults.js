c874a8ae7390a9863f702dcf10193aec75e9051c
alexcolbeck1 <36191568+alexcolbeck1@users.noreply.github.com>
2024-12-19 10:40:13 +0000

    Updating default session data

--- app/data/session-data-defaults.js
+++ app/data/session-data-defaults.js
@@ -26,6 +26,7 @@ module.exports = {
     "checkbox-accessible-letter": "",
     "messages-draft": "0",
     "messages-prod": "0",
-    "channel-amount": "0"
+    "channel-amount": "1",
+    "channel-1-type": "NHS App"
   
   }
