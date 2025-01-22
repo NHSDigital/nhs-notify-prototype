758ed0ec10980f216b191d22b655359300cd4a89
alexcolbeck1 <36191568+alexcolbeck1@users.noreply.github.com>
2025-01-22 10:51:05 +0000

    Latest content amends and example template names added

--- app/data/session-data-defaults.js
+++ app/data/session-data-defaults.js
@@ -1,21 +1,20 @@
 module.exports = {
 
     "folders": "0",
-    "folder-name-1": "My project name",
 
     "folder-1-app-templates": "0",
     "folder-1-email-templates": "0",
     "folder-1-sms-templates": "0",
     "folder-1-letter-templates": "0",
 
-    "template-app-name-1": "South West region 40 to 60 year olds",
-    "template-app-message-1": "# Lorem ipsum dolor sit amet\n\nConsectetur adipiscing elit. Duis lacinia tellus nunc, et pretium dui mollis sed. Aliquam metus nulla, ornare non gravida a, congue id mauris.\n\n* Phasellus eget vulputate dui\n* Nec gravida dui\n* Phasellus sollicitudin\n\nPhasellus eget vulputate dui, nec gravida dui. Phasellus sollicitudin, dui vitae vestibulum efficitur, nisi justo bibendum ipsum, rhoncus tincidunt quam lacus sed elit. Suspendisse sed quam at quam mattis consectetur.\n\n**Vestibulum eros diam**\n\nElementum ac nisl ut, sollicitudin volutpat dui. Proin bibendum justo ultricies malesuada faucibus. Maecenas eget nulla ut turpis congue viverra. Suspendisse cursus lacus ut dui dignissim dictum.\n\nhttps://www.nhs.uk/\n\nMorbi id nisi faucibus, convallis metus eu, volutpat dolor. Suspendisse auctor dolor vel lectus venenatis, vitae facilisis est dictum.\n\nVestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Duis bibendum lacus sodales diam vulputate facilisis.",
+    "template-app-name-1": "An example NHS App template",
+    "template-app-message-1": "# Here is a main heading\n\nConsectetur adipiscing elit. Duis lacinia tellus nunc, et pretium dui mollis sed. Aliquam metus nulla, ornare non gravida a, congue id mauris.\n\n* Phasellus eget vulputate dui\n* Nec gravida dui\n* Phasellus sollicitudin\n\nPhasellus eget vulputate dui, nec gravida dui. Phasellus sollicitudin, dui vitae vestibulum efficitur, nisi justo bibendum ipsum, rhoncus tincidunt quam lacus sed elit. Suspendisse sed quam at quam mattis consectetur.\n\n**Vestibulum eros diam**\n\nElementum ac nisl ut, sollicitudin volutpat dui. Proin bibendum justo ultricies malesuada faucibus. Maecenas eget nulla ut turpis congue viverra. Suspendisse cursus lacus ut dui dignissim dictum.\n\nhttps://www.nhs.uk/\n\nMorbi id nisi faucibus, convallis metus eu, volutpat dolor. Suspendisse auctor dolor vel lectus venenatis, vitae facilisis est dictum.\n\nVestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Duis bibendum lacus sodales diam vulputate facilisis.",
 
-    "template-email-name-1": "National over 16s cohort",
+    "template-email-name-1": "An email template for testing",
     "template-email-subject-1": "The subject line for the email",
-    "template-email-message-1": "# Lorem ipsum dolor sit amet\n\nConsectetur adipiscing elit. Duis lacinia tellus nunc, et pretium dui mollis sed. Aliquam metus nulla, ornare non gravida a, congue id mauris.\n\n* Phasellus eget vulputate dui\n* Nec gravida dui\n* Phasellus sollicitudin\n\nPhasellus eget vulputate dui, nec gravida dui. Phasellus sollicitudin, dui vitae vestibulum efficitur, nisi justo bibendum ipsum, rhoncus tincidunt quam lacus sed elit. Suspendisse sed quam at quam mattis consectetur.\n\n**Vestibulum eros diam**\n\nElementum ac nisl ut, sollicitudin volutpat dui. Proin bibendum justo ultricies malesuada faucibus. Maecenas eget nulla ut turpis congue viverra. Suspendisse cursus lacus ut dui dignissim dictum.\n\nhttps://www.nhs.uk/\n\nMorbi id nisi faucibus, convallis metus eu, volutpat dolor. Suspendisse auctor dolor vel lectus venenatis, vitae facilisis est dictum.\n\n---\n\nVestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Duis bibendum lacus sodales diam vulputate facilisis.",
+    "template-email-message-1": "# Here is a main heading\n\nConsectetur adipiscing elit. Duis lacinia tellus nunc, et pretium dui mollis sed. Aliquam metus nulla, ornare non gravida a, congue id mauris.\n\n* Phasellus eget vulputate dui\n* Nec gravida dui\n* Phasellus sollicitudin\n\nPhasellus eget vulputate dui, nec gravida dui. Phasellus sollicitudin, dui vitae vestibulum efficitur, nisi justo bibendum ipsum, rhoncus tincidunt quam lacus sed elit. Suspendisse sed quam at quam mattis consectetur.\n\n**Vestibulum eros diam**\n\nElementum ac nisl ut, sollicitudin volutpat dui. Proin bibendum justo ultricies malesuada faucibus. Maecenas eget nulla ut turpis congue viverra. Suspendisse cursus lacus ut dui dignissim dictum.\n\nhttps://www.nhs.uk/\n\nMorbi id nisi faucibus, convallis metus eu, volutpat dolor. Suspendisse auctor dolor vel lectus venenatis, vitae facilisis est dictum.\n\n---\n\nVestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Duis bibendum lacus sodales diam vulputate facilisis.",
 
-    "template-sms-name-1": "North East region over 16s",
+    "template-sms-name-1": "An example text message template",
     "template-sms-message-1": "Consectetur adipiscing elit. Duis lacinia tellus nunc, et pretium dui mollis sed. Aliquam metus nulla, ornare non gravida a, congue id mauris. Aliquam metus.",
 
     "template-letter-name-1": "National 20 to 40 year olds",
@@ -24,27 +23,37 @@ module.exports = {
     "template-letter-message-1": "Here is the message body.",
 
     "checkbox-accessible-letter": "",
+
+    "folder-name-1": "Autumn / winter 2024 campaign",
+
     "messages-draft": "0",
     "messages-prod": "0",
     "channel-amount": "0",
+
     "channel-1-template": "",
     "channel-2-template": "",
     "channel-3-template": "",
-    "template-1-name": "My test template 1",
-    "template-2-name": "Another test template 2",
-    "template-3-name": "A template for testing 3",
-    "template-4-name": "My test template 4",
-    "template-5-name": "Another test template 5",
-    "template-6-name": "A template for testing 6",
-    "template-7-name": "My test template 7",
-    "template-8-name": "Another test template 8",
-    "template-9-name": "A template for testing 9",
-    "template-10-name": "Another test template 10",
-    "template-11-name": "A template for testing 11",
-    "template-12-name": "My test template 12",
-    "template-13-name": "My test template 13",
-    "template-14-name": "A template for testing 14",
-    "template-folder-1-name": "A folder to put templates in 1",
-    "template-folder-2-name": "Another folder 2"
+    
+    "template-1-name": "Covid 65 plus invitation english v1 (T01)", // App
+    "template-2-name": "Covid 65 plus english v1 (T02)", // App
+    "template-3-name": "Covid 65 plus english v2 (T03)", // App
+
+    "template-4-name": "Covid 65 plus invitation english v1 (T04)", // SMS
+    "template-5-name": "Covid 65 plus english v1 (T05)", // SMS
+    "template-6-name": "Covid 65 plus english v2 (T06)", // SMS
+
+    "template-7-name": "Covid 65 plus invitation english v1 (T07)", // Email
+    "template-8-name": "Covid 65 plus english v1 (T08)", // Email
+    "template-9-name": "Covid 65 plus english v2 (T09)", // Email
+
+    "template-10-name": "Covid 16-64 english v1 (T10)", // App + folder 1
+    "template-11-name": "Covid 16-64 english v1 (T11)", // Email + folder 1
+    "template-12-name": "Covid 16-64 english v1 (T12)", // SMS + folder 1
+
+    "template-13-name": "RSV awareness 75-79 english v1 (T13)", // App + folder 2
+    "template-14-name": "RSV awareness 75-79 english v1 (T14)", // Email + folder 2
+    
+    "template-folder-1-name": "Covid Sep 2024",
+    "template-folder-2-name": "RSV awareness Sep 2024"
   
   }
