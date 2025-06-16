0feb5928190a870d793ac92c01d0c0a4201ea090
alexcolbeck1 <36191568+alexcolbeck1@users.noreply.github.com>
2025-06-16 12:00:29 +0100

    Changing template guidance column to show by default

--- app/assets/javascript/guidanceToggle3.js
+++ app/assets/javascript/guidanceToggle3.js
@@ -1,19 +1,17 @@
 
-// Open the guidance column if the user has left it open during their session
+// Close the guidance column if the user has left it closed during their session
 window.addEventListener("load", (event) => {
 
 var columnRight = document.getElementById("column-right")
 var columnLeft = document.getElementById("column-left")
 var guidance = sessionStorage.getItem("guidance")
 
-if (guidance === "open") {
-    columnLeft.classList.add("nhsuk-grid-column-two-thirds")
-    columnLeft.classList.remove("nhsuk-grid-column-full")
-    columnRight.style.display = "block"
-    button.innerHTML = "Hide formatting &amp; personalisation guidance"
-    columnRight.removeAttribute("aria-hidden")
-} else {
+if (guidance === "closed") {
+    columnLeft.classList.remove("nhsuk-grid-column-two-thirds")
+    columnLeft.classList.add("nhsuk-grid-column-full")
     columnRight.style.display = "none"
+    button.innerHTML = "Show formatting and personalisation guidance"
+    columnRight.addAttribute("aria-hidden")
 }
 
 });
@@ -30,23 +28,23 @@ function guidanceShowHideV2() {
     var guidance = sessionStorage.getItem("guidance")
 
     // Hide
-    if (guidance === "open") {
-        columnLeft.classList.remove("nhsuk-grid-column-two-thirds")
-        columnLeft.classList.add("nhsuk-grid-column-full")
-        columnRight.style.display = "none"
-        button.innerHTML = "Show formatting &amp; personalisation guidance"
-        columnRight.setAttribute("aria-hidden", "true")
-        sessionStorage.setItem("guidance", "closed")
+    if (guidance === "closed") {
+        columnLeft.classList.add("nhsuk-grid-column-two-thirds")
+        columnLeft.classList.remove("nhsuk-grid-column-full")
+        columnRight.style.display = "block"
+        button.innerHTML = "Hide formatting and personalisation guidance"
+        columnRight.removeAttribute("aria-hidden")
+        sessionStorage.setItem("guidance", "open")
     } 
     
     // Show
     else {
-        columnLeft.classList.add("nhsuk-grid-column-two-thirds")
-        columnLeft.classList.remove("nhsuk-grid-column-full")
-        columnRight.style.display = "block"
-        button.innerHTML = "Hide formatting &amp; personalisation guidance"
-        columnRight.removeAttribute("aria-hidden")
-        sessionStorage.setItem("guidance", "open")
+        columnLeft.classList.remove("nhsuk-grid-column-two-thirds")
+        columnLeft.classList.add("nhsuk-grid-column-full")
+        columnRight.style.display = "none"
+        button.innerHTML = "Show formatting and personalisation guidance"
+        columnRight.setAttribute("aria-hidden", "true")
+        sessionStorage.setItem("guidance", "closed")
     }
 
     return false;
